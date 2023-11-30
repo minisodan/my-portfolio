17569206188c32fb0e99f98e29fa2045694388e7
Mason Myles <mason.myles98@gmail.com>
2023-11-30 13:39:49 -0600

    test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
-            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/portfolio" exact element={<Portfolio />} />
             <Route path="/contact" element={<Contact />} />
           </Routes>
         </Router>
--- src/Componenets/pages/Portfolio.jsx
+++ src/Componenets/pages/Portfolio.jsx
@@ -1,9 +1,28 @@
+import { useState, useEffect } from "react";
 import "./Portfolio.css";
+import axios from 'axios';
 
 function Portfolio() {
+  const [repos, setRepos] = useState([]);
+  const axios = require('axios');
+
+  useEffect(() => {
+    axios.get('https://api.github.com/users/minisodan/repos', {
+      Authorization: `Client-ID `,
+    })
+    .then((responceData) => {
+      const repo = responceData.data;
+      console.log(repo);
+
+    })
+  }, []);
+
+
   return (
     <div className="parent">
-      <h1>!Currently Under Construction!</h1>
+      <div className="repos">
+      {repos}
+      </div>
     </div>
   );
 }
