7994776fd53d50c7997a53c3d00fecbc250a69b1
Mason Myles <59033177+minisodan@users.noreply.github.com>
2023-12-12 23:34:51 -0600

    learning how to use fetch with js.

--- src/Componenets/pages/Portfolio.jsx
+++ src/Componenets/pages/Portfolio.jsx
@@ -1,11 +1,30 @@
+import { useEffect, useState } from "react";
 import "./Portfolio.css";
 
 function Portfolio() {
+  const [repo, setRepo] = useState();
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/minisodan/repos", {
+      method: "GET",
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw response;
+      })
+      .then((data) => {
+        setRepo(data);
+        console.log(data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <div className="parent">
       <h1>!Currently Under Construction!</h1>
     </div>
   );
 }
-
 export default Portfolio;
