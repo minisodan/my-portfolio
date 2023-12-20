beb4e3683c5d92f9c8c846c2a96c1448a6f06198
Mason Myles <59033177+minisodan@users.noreply.github.com>
2023-12-19 23:58:42 -0600

    Cards (#3)
    * test
    
    * added links to the repos with names. still need to style the page
    
    ---------
    
    Co-authored-by: Mason Myles <mason.myles98@gmail.com>

--- src/Componenets/pages/Portfolio.jsx
+++ src/Componenets/pages/Portfolio.jsx
@@ -1,31 +1,39 @@
 import { useEffect, useState } from "react";
-import "./Portfolio.css";
 
-function Portfolio() {
-  const [repo, setRepo] = useState();
+import "./Styles/Portfolio.css";
 
+const Portfolio = () => {
+  const [repos, setRepo] = useState([]);
+
+  const fetchRepos = async() => {
+    const response = await fetch("https://api.github.com/users/minisodan/repos");
+    const repos = await response.json();
+    setRepo(repos);
+    console.log(repos)
+  }
   useEffect(() => {
-    fetch("https://api.github.com/users/minisodan/repos", {
-      method: "GET",
-    })
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        }
-        throw response;
-      })
-      .then((data) => {
-        setRepo(data);
-        console.log(data);
-      })
-      .catch((error) => console.log(error));
+    fetchRepos();
   }, []);
 
 
   return (
-    <div className="parent">
-      <h1>!Currently Under Construction!</h1>
-    </div>
+    <>
+      <h3>github users</h3>
+      <ul className='users'>
+        {repos.map((user) => {
+          const { id, name, html_url } = user;
+          return (
+            <li key={id}>
+              <div>
+                <h4>{name}</h4>
+                <a href={html_url}>profile</a>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </>
   );
 }
+
 export default Portfolio;
