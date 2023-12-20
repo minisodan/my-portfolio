2e2fb4089bc6c7853a320278a4c1ad8847ed5c10
Mason Myles <mason.myles98@gmail.com>
2023-12-19 23:55:17 -0600

    added links to the repos with names. still need to style the page

--- src/Componenets/pages/Portfolio.jsx
+++ src/Componenets/pages/Portfolio.jsx
@@ -1,30 +1,37 @@
-import { useState, useEffect } from "react";
-import "./Portfolio.css";
-import axios from 'axios';
+import { useEffect, useState } from "react";
+import "./Styles/Portfolio.css";
 
-function Portfolio() {
-  const [repos, setRepos] = useState([]);
-  const axios = require('axios');
+const Portfolio = () => {
+  const [repos, setRepo] = useState([]);
 
+  const fetchRepos = async() => {
+    const response = await fetch("https://api.github.com/users/minisodan/repos");
+    const repos = await response.json();
+    setRepo(repos);
+    console.log(repos)
+  }
   useEffect(() => {
-    axios.get('https://api.github.com/users/minisodan/repos', {
-      Authorization: `Client-ID `,
-    })
-    .then((responceData) => {
-      const repo = responceData.data;
-      console.log(repo);
-
-    })
+    fetchRepos();
   }, []);
 
 
   return (
-    <div className="parent">
-      <div className="repos">
-      {repos}
-      </div>
-    </div>
+    <>
+      <h3>github users</h3>
+      <ul className='users'>
+        {repos.map((user) => {
+          const { id, name, html_url } = user;
+          return (
+            <li key={id}>
+              <div>
+                <h4>{name}</h4>
+                <a href={html_url}>profile</a>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </>
   );
 }
-
 export default Portfolio;
