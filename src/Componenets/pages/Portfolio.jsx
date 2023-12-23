466025bbd54dc31531b77b953c24785c044753c2
Mason Myles <mason.myles98@gmail.com>
2023-12-23 03:24:43 -0600

    added styling to cards

--- src/Componenets/pages/Portfolio.jsx
+++ src/Componenets/pages/Portfolio.jsx
@@ -17,21 +17,26 @@ const Portfolio = () => {
 
   return (
     <>
-      <h3>github users</h3>
-      <ul className='users'>
-        {repos.map((user) => {
-          const { id, name, html_url } = user;
-          return (
-            <li key={id}>
-              <div>
-                <h4>{name}</h4>
-                <a href={html_url}>profile</a>
-              </div>
-            </li>
-          );
-        })}
-      </ul>
-    </>
+    <div className="repoCards">
+      <div className="repoCard">
+        <h3>Projects</h3>
+          <ul className='users'>
+            {repos.map((user) => {
+              const { id, name, html_url, language, avatar_url } = user;
+              return (
+                <li key={id}>
+                  <div>
+                    <h4>{name}</h4>
+                    <img src= {avatar_url}/>
+                    <p>Language Used: {language}</p>
+                    <a href={html_url}>Link to Repo</a>
+                  </div>
+                </li>
+              )})}
+          </ul>
+      </div>
+    </div>
+  </>
   );
 }
 export default Portfolio;
