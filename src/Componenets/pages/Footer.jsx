bfcc681926beed70c30e024fb2d08b7a174e61b9
Mason Myles <mason.myles98@gmail.com>
2023-12-24 13:07:22 -0600

    fixing footer issue, setting footer to bottom of page absolute

--- src/Componenets/pages/Footer.jsx
+++ src/Componenets/pages/Footer.jsx
@@ -4,23 +4,27 @@ import "./Styles/Footer.css";
 
 function Footer() {
   return (
-    <footer className="Footer">
-      <div className="icons">
-        <ul>
-          <li>
-            <a href="https://www.linkedin.com/in/masonmyles/">
-              <FontAwesomeIcon icon={faLinkedin} />
-            </a>
-          </li>
+    <>
+    <div className="footer">
+      <footer className="Footer">
+        <div className="icons">
+          <ul>
+            <li>
+              <a href="https://www.linkedin.com/in/masonmyles/">
+                <FontAwesomeIcon icon={faLinkedin} />
+              </a>
+            </li>
 
-          <li>
-            <a href="https://github.com/minisodan">
-              <FontAwesomeIcon icon={faGithub} />
-            </a>
-          </li>
-        </ul>
-      </div>
-    </footer>
+            <li>
+              <a href="https://github.com/minisodan">
+                <FontAwesomeIcon icon={faGithub} />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </footer>
+    </div>
+    </>
   );
 }
 
