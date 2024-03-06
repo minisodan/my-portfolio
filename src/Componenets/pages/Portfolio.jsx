a763cfa61a2bd16d3ef23914d3c8798b4a1763c6
Mason Myles <59033177+minisodan@users.noreply.github.com>
2024-03-05 21:46:15 -0600

    GitHub api (#9)
    * learning how to use fetch with js.
    
    * edit work so name fits

--- src/Componenets/pages/Portfolio.jsx
+++ src/Componenets/pages/Portfolio.jsx
@@ -4,43 +4,46 @@ import "./Styles/Portfolio.css";
 const Portfolio = () => {
   const [repos, setRepo] = useState([]);
 
-  const fetchRepos = async() => {
-    const response = await fetch("https://api.github.com/users/minisodan/repos");
+  const fetchRepos = async () => {
+    const response = await fetch(
+      "https://api.github.com/users/minisodan/repos"
+    );
     const repos = await response.json();
     setRepo(repos);
-    console.log(repos)
-  }
+    console.log(repos);
+  };
   useEffect(() => {
     fetchRepos();
   }, []);
 
-
   /** make new branch */
   return (
     <>
-    <div className="repoCards">
-      <div className="repoCard">
-        <h3>Projects</h3>
-        <br></br>
-          <ul className='users'>
+      <div className="repoCards">
+        <div className="repoCard">
+          <h3>Projects</h3>
+          <br></br>
+          <ul className="users">
             {repos.map((user) => {
               const { id, name, html_url, language, owner } = user;
               return (
                 <li key={id}>
-                  <div className="gitHubContent">
-                  <img src={`${owner.avatar_url}`} alt="AvatarPFP"/>
-                    <h4>Project Name: {name}</h4>
-                    <p>Language Used: {language}</p>
-                    <br></br>
-                    <br></br>
-                    <a href={html_url}>Link to Repo</a>
+                  <div className="">
+                    <div className="gitHubContent">
+                      <img src={`${owner.avatar_url}`} alt="AvatarPFP" />
+                      <h4>Project Name: {name}</h4>
+                      <p>Language Used: {language}</p>
+                      <br></br>
+                      <a href={html_url}>Link to Repo</a>
+                    </div>
                   </div>
                 </li>
-              )})}
+              );
+            })}
           </ul>
+        </div>
       </div>
-    </div>
-  </>
+    </>
   );
-}
+};
 export default Portfolio;
