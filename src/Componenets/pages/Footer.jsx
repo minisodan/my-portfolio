012826fdccf4624dc3193fb89aeeff6e50a47d97
Mason Myles <mason.myles98@gmail.com>
2024-03-21 16:46:25 -0500

    adding more tailwind for mobile applications

--- src/Componenets/pages/Footer.jsx
+++ src/Componenets/pages/Footer.jsx
@@ -5,8 +5,8 @@ function Footer() {
   return (
     <>
       <footer class="flex rounded-lg shadow m-4 bg-transparent">
-        <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-          <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+        <div class="md:w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+          <p class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
             © 2024{" "}
             <a
               href="https://my-portfolio-psi-plum.vercel.app"
@@ -15,7 +15,7 @@ function Footer() {
               Mason Myles
             </a>
             . All Rights Reserved.
-          </span>
+          </p>
           <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
             <li>
               <a href="https://www.linkedin.com/in/masonmyles/">
